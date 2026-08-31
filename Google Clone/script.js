9170b29663777c5796f4254039d296d357ac048f
cody-by-om <ompanda648@gmail.com>
2026-08-31 18:36:20 +0530

    Create script.js

---
+++ Google Clone/script.js
@@ -0,0 +1,12 @@
+let searchQuery = "JavaScript";
+
+function search(query) {
+
+    if (query === "") {
+        console.log("Please enter something.");
+    } else {
+        console.log("Searching for: " + query);
+    }
+}
+
+search(searchQuery);
